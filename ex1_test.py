49c7e8a1d6a766511066c1c401d4d9a51908b740
Yat Chit Law <85443779+yatchit888@users.noreply.github.com>
2024-01-09 22:19:13 -0500

    Add files via upload

---
+++ ex1_test.py
@@ -0,0 +1,7 @@
+import write_your_name as name_point
+
+def test_hi_my_name_is():
+    assert len(name_point.hi_my_name_is())  > 1 
+
+
+
---
+++ write_your_name.py
@@ -0,0 +1,2 @@
+def hi_my_name_is():
+    return "Yat Chit Law" # Write your name inside the quotation marks
